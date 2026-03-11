c3103954801ca2e1b2d4ceb3896827863c709abf
JFLarvoire <jf.larvoire@free.fr>
2026-03-11 11:30:32 +0100

    Use NMaker as a git submodule

--- configure.ag.bat
+++ configure.ag.bat
@@ -9,6 +9,7 @@
 :#                                                                            *
 :#  History:                                                                  *
 :#   2017-03-04 JFL Created this file.					      *
+:#   2026-03-11 JFL Define the full pathname of the STINCLUDE subdirectory.   *
 :#                                                                            *
 :#*****************************************************************************
 
@@ -20,6 +21,12 @@ if not defined OUTDIR ( :# Unless already redirected elsewhere
 :# Avoid trying to build ag.exe for DOS and WIN95, which are not supported yet.
 if "%OS%"=="Windows_NT" set "OS=" &:# This is the Windows' homonym default, same as undefined for us here.
 if not defined OS set "OS=WIN32 WIN64" &:# Only WIN32 and WIN64 are supported so far.
+
+:# Get the full pathname of the include subdirectories
+if defined NMINCLUDE if not exist "%NMINCLUDE%\debugm.h" set "NMINCLUDE=" &:# Allow overriding with another instance, but ignore invalid overrides
+if not defined NMINCLUDE set "NMINCLUDE=%~dp0\win32\NMaker\include" &:# Default: Use the local instance
+if defined STINCLUDE if not exist "%STINCLUDE%\dict.h" set "STINCLUDE=" &:# Allow overriding with another instance, but ignore invalid overrides
+if not defined STINCLUDE set "STINCLUDE=%~dp0\win32\include" &:# Default: Use the local instance
 
 :# We need the 7-Zip LZMA SDK, from https://www.7-zip.org/sdk.html
 %USE_SDK% LZMA
